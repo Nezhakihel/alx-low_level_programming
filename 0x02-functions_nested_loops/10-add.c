deec4f8ac1593094132a75e5f29ecdcef0b22502
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-20 07:26:22 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "holberton.h"
+
+/**
+ * add - Entry point
+ * @a: variable 1
+ * @b: variable 2
+ * Return: result of addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
