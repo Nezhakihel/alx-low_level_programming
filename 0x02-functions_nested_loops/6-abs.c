21b747fef9a16d574d106c0d00acd148fd83dbd2
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-20 04:04:58 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: the variable to comput
+ * Return: computes the absolute value of an integer
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
