2eb1a19c1bec4566c64462c54564e75e8c9d8468
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-20 02:12:01 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char character;
+	int i = 0;
+
+	while (i < 10)
+	{
+		character = 'a';
+		while (character <= 'z')
+		{
+			_putchar(character);
+			character++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
