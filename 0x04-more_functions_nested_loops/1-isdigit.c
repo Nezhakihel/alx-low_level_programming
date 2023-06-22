10f4b695503e34dac83504f742a4871128e6bdca
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-22 09:54:41 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - Entry point
+ * _isdigit - Entry point
  * @c: character to check
  * Return: 0 or 1
  */
 
-int _isupper(int c)
-
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
-
 }
