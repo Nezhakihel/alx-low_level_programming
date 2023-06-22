96acba05a55cc08f9811036d5d94d028c444c3ef
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-22 11:02:06 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,26 +12,18 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0 || x % 5 == 0)
-		{
-			if (x % 3 == 0)
-			{
-				printf("Fizz");
-			}
-			if (x % 5 == 0)
-			{
-				printf("Buzz");
-			}
-		}
+		if (x % 3 == 0 && !(x % 5 == 0))
+			printf("Fizz");
+		else if (x % 5 && !(x % 3 == 0))
+			printf("Buzz");
+		else if (x% 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
 		else
-		{
 			printf("%d", x);
-		}
 		if (x != 100)
-		{
-			putchar(' ');
-		}
+			printf(" ");
+		else
+			printf("\n");
 	}
-	putchar('\n');
 	return (0);
 }
