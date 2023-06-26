95105a4b656c95c125ac4f13f965ad9845568577
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-26 08:00:08 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,20 +10,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
-/**
- * main - check the code
- * n: variable to update
- * Return: Always 0
- */
-
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
