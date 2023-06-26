3279dff06325f3b350474d49743c94501f2ca681
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-26 14:29:41 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	char s_reverse[length];
+	int j = 0;
+
+	for (int i = length-1; i >= 0; i--)
+	{
+		s_reverse[j] = s[i];
+		j++;
+	}
+	s  = s_reverse;
+	printf("%s\n", s_reverse);
+}
