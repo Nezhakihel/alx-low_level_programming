338ef0692ea2f67313c2ce29784fa9b1d61c534a
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-30 05:52:13 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+/**
+ * infinite_add - function add to nb
+ * @n1: nb1
+ * @n2: nb2
+ * @r : buffer that the function will use to store the result
+ * @size_r: buffer size
+ * Return : pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0;
+	int j = 0;
+
+	/* size of n1 */
+	while (n1[i] != 0)
+		i++;
+	/* size of n2 */
+	while (n2[j] != 0)
+		j++;
+	/* if size of n1 || n2 less than size_r return 0 */
+	if (i - 1 > size_r || j - 1 > size_r)
+		return (0);
+	else
+		return (n1 + n2);
+}
