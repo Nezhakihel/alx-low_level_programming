9dcb8714a824a7cb7d48bfddc06911908108c0b9
Nezhakihel <nezhaelkihelg@gmail.com>
2023-06-29 17:17:32 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: pointer
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= '2'))
+		{
+			if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}' || index == 0)
+			{
+				str[index] -= 32;
+			}
+			index++;
+		}
+		return (str);
+	}
+}
