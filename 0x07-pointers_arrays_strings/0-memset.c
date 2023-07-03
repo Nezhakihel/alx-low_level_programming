88c5d567cc0d42f6c7868cb0ce85e5a8053809f8
Nezhakihel <nezhaelkihelg@gmail.com>
2023-07-03 10:23:03 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,10 +14,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
-		i++;
+		*(s + i) = b; /*add 1 position s*/
+
 	}
 	return (s);
 }
