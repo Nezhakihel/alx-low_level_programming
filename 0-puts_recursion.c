55e06559aeb5f21eaea52cf3aaefd823caf6536b
Nezhakihel <nezhaelkihelg@gmail.com>
2023-07-05 10:47:04 -0700

    0-puts_recursion.c

---
+++ 0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string
+ * s: pointer contains string
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s", *s);
+}
