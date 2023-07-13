7adbd54111c09d23c1974cd1636d7ff1ceac3b08
Nezhakihel <nezhaelkihelg@gmail.com>
2023-07-13 16:21:19 -0700

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,11 +3,13 @@
 
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int len(char *str);
 int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
