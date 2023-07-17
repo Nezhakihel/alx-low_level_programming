5b0cc060f81e78ebd79e8a46f74e0f2235bd362e
Nezhakihel <nezhaelkihelg@gmail.com>
2023-07-17 12:01:53 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,12 +7,15 @@
  * @name: pointer to name
  * @age: pointer to age
  * @owner: pointer to owner
- * description: Initialize dog functione
+ * Return: Nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
