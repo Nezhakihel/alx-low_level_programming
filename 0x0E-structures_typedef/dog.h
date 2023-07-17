f1df024c10d5ca0d8b81990b4f616bc76db5b036
Nezhakihel <nezhaelkihelg@gmail.com>
2023-07-17 10:51:35 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Struct dog function
+ * @name: name as a variable as char type
+ * @age: age as a variable astype float
+ * @owner: owner as a variable of char type
+ * Description: A struct dog function
+ */
 struct dog
 {
 	char *name;
