d992d28bd7afbd0c5b5a3a71399198d5c7ee4be6
Nezhakihel <nezhaelkihelg@gmail.com>
2023-07-19 13:36:27 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
+ * print_name - prints a name
+ * @f: function pointer
+ * @name: name of a person
  * Return: Nothing.
  */
 
