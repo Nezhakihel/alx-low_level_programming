080c7fa04579d8f597661b026083a11a530dc85a
nezha el <nezhaelkihelg@gmail.com>
2023-08-01 08:39:24 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * listint_len - Returns the number of elements in a linked listint_t list
- * @h: A pointer to the head of the list 
+ * @h: A pointer to the head of the list
  * Return: the number of elements in the listint_t list
  */
 
