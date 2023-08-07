15e6b8571856fc1f1c671a6f76d94427ac9da988
nezha el <nezhaelkihelg@gmail.com>
2023-08-07 14:17:01 -0700

    update2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+
 /**
  * create_file - creates a file
  * @filename: name of the file to create
@@ -11,26 +9,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd_open, fd_write, count_lettres = 0;
-	/* if filename is NULL return -1 */
+	int fd_open, fd_write, count = 0;
+
 	if (filename == NULL)
 		return (-1);
-	/* open the file */
-	fd_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	/* if file can not be created */
+	fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd_open == -1)
 		return (-1);
-	/* count letters */
 	while (*text_content++)
-		count_lettres++;
-	/**/
+		count++;
 	if (text_content != NULL)
 	{
-		fd_write = write(fd_open, text_content, count_lettres);
+		fd_write = write(fd_open, text_content, count);
 		if (fd_write == -1)
 			return (-1);
 	}
-	/**/
 	close(fd_open);
 	return (1);
 }
