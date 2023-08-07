b1636bb990132353c6af538b40c72bc6fd474988
nezha el <nezhaelkihelg@gmail.com>
2023-08-07 14:56:16 -0700

    update4

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,14 +14,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
-	if (text_content)
+	if (text_content != NULL)
 	{
 		while (text_content[i])
 			i++;
-		fd_write = write(fild, text_content, i);
-		if (fd_write < 0)
+		fd_write = write(fd, text_content, i);
+		if (fd_write == -1)
 			return (-1);
 	}
 	close(fd);
