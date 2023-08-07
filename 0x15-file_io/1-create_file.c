92b12ec7e15e1aa4b3f42448a875231730fb6ebb
nezha el <nezhaelkihelg@gmail.com>
2023-08-07 14:36:01 -0700

    update3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,10 +6,12 @@
  * @text_content: text to write inside the file.
  * Return: 1 if the file was created, -1 otherwise.
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd_open, fd_write;
-
+	int count = 0;
+	/**/
 	if (filename == NULL)
 		return (-1);
 	fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
@@ -17,24 +19,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
+		while (*text_content++)
+			count++;
 		fd_write = write(fd_open, text_content, _strlen(text_content));
 		if (fd_write == -1)
 			return (-1);
 	}
 	close(fd_open);
 	return (1);
-}
-
-/**
- * _strlen - calculate the lenght of a string.
- * @str: array of characters
- * Return: lenght of the string
- */
-int _strlen(char *str)
-{
-	int count = 0;
-
-	while (*str++)
-		count++;
-	return (count);
 }
