fc8523952150002f8406621dd114d9c5c48ef9a6
nezha el <nezhaelkihelg@gmail.com>
2023-08-07 14:40:02 -0700

    update3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (*text_content++)
+		while (text_content[count])
 			count++;
-		fd_write = write(fd_open, text_content, _strlen(text_content));
+		fd_write = write(fd_open, text_content, count);
 		if (fd_write == -1)
 			return (-1);
 	}
