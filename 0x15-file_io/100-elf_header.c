fd63e3ce8eb6a83fc2e372e5ebd3ede576610598
nezha el <nezhaelkihelg@gmail.com>
2023-08-10 16:01:06 -0700

    update

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * main -  displays the information contained in the ELF header at the start of an ELF file.
+ * @argc: var
+ * @argv: pointer
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int open, read;
+	Elf64_Ehdr *header;
+
+	if (argc != 2)
+	{
+		printf("Usage: elf_header elf_filename\n");
+		return (1);
+	}
+	open = open(argv[1], O_RDONLY);
+	if (open == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(open);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	read = read(open, header, sizeof(Elf64_Ehdr));
+	if (read == -1)
+	{
+		free(header);
+		close_elf(open);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
+	if (header->e_ident[0] != 0x7f || header->e_ident[1] != 'E' || header->e_ident[2] != 'L' || header->e_ident[3] != 'F')
+	{
+		dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+	}
+	printf("ELF Header:\n");
+	print_magic(header->e_ident);
+	print_class(header->e_ident);
+	print_data(header->e_ident);
+	print_version(header->e_ident);
+	print_osAbi(header->e_ident);
+	print_abiVersion(header->e_ident);
+	print_type(header->e_type, header->e_ident);
+	print_entryPointAdress(header->e_entry, header->e_ident);
+	free(header);
+	close_elf(open);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include<fcntl.h>
+#include <elf.h>
 
 #define BUFSIZE 1024
 
@@ -12,5 +13,15 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abiVersion(unsigned char *e_ident);
+void print_osAbi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entryPointAdress(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
