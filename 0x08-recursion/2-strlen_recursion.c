7d676dddff4139e951ab01dae8a7b31a1f8a52b8
nezha el <nezhaelkihelg@gmail.com>
2023-08-30 11:37:35 -0700

    strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -returns the length of a string
+ * @s: string
+ * Return: returns the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len = _strlen_recursion(s + 1);
+		return(len + 1)
+	}
+	return(0);
+}
